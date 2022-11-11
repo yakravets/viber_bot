345b4558f7a8036df43db72e2a2c13719528d83b
yakravets <yar.kravets@ukr.net>
2022-11-11 15:57:07 +0200

    add start feature

---
+++ ViberSender/Controllers/ViberController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Viber.Bot.NetCore.Infrastructure;
+using Viber.Bot.NetCore.Models;
+using Viber.Bot.NetCore.RestApi;
+
+namespace ViberSender.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ViberController : ControllerBase
+    {
+        private readonly IViberBotApi _viberBotApi;
+        private readonly IConfiguration _configuration;
+
+        public ViberController(IViberBotApi viberBotApi, IConfiguration configuration)
+        {
+            _viberBotApi = viberBotApi;
+            _configuration = configuration;
+        }
+
+        // The service sets a webhook automatically, but if you want sets him manually then use this
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var webhookUrl = _configuration.GetSection("ViberBot")["Webhook"];
+            var response = await _viberBotApi.SetWebHookAsync(new ViberWebHook.WebHookRequest(webhookUrl));
+
+            if (response.Content?.Status == ViberErrorCode.Ok)
+            {
+                return Ok("Viber-bot is active");
+            }
+            else
+            {
+                return BadRequest(response.Content?.StatusMessage);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] ViberCallbackData update)
+        {
+            var str = String.Empty;
+
+            switch (update.Message.Type)
+            {
+                case ViberMessageType.Text:
+                    {
+                        var mess = update.Message as ViberMessage.TextMessage;
+
+                        str = mess.Text;
+
+                        break;
+                    }
+
+                default: break;
+            }
+
+            // you should to control required fields
+            var message = new ViberMessage.TextMessage()
+            {
+                //required
+                Receiver = update.Sender.Id,
+                Sender = new ViberUser.User()
+                {
+                    //required
+                    Name = "Our bot",
+                    Avatar = "http://dl-media.viber.com/1/share/2/long/bots/generic-avatar%402x.png"
+                },
+                //required
+                Text = str
+            };
+
+            // our bot returns incoming text
+            var response = await _viberBotApi.SendMessageAsync<ViberResponse.SendMessageResponse>(message);
+
+            return Ok();
+        }
+    }
+}
---
+++ ViberSender/Program.cs
@@ -0,0 +1,35 @@
+using Viber.Bot.NetCore.Middleware;
+
+namespace ViberSender
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Viber DI.
+            builder.Services.AddViberBotApi(builder.Configuration);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
